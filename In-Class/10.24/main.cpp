b616693d8a034b57c60e96fc3ac824219d8d54f4
ethan-leonard <ethan.leonard821@gmail.com>
2024-10-24 10:26:18 -0700

    update 10.22

--- In-Class/10.24/main.cpp
+++ In-Class/10.24/main.cpp
@@ -28,7 +28,7 @@ bool printBase(int num, int base, std::string &answer) {
     } return true;
 }
 
-Bird* createFlock(int num, std::string name) {
+/* Bird* createFlock(int num, std::string name) {
     Bird* pBird;
     pBird = new Bird[num];
 
@@ -37,13 +37,31 @@ Bird* createFlock(int num, std::string name) {
         pBird[i].set_id(i);
     }
     return pBird;
+} */
+
+int binarySearch(int arr[], int x, int min, int max) {
+    while (min <= max) {
+        int mid = (min + max) / 2;
+        if (x == arr[mid]) {
+            return mid;
+        } else if (x > arr[mid]) {
+            min = mid + 1;
+        } else if (x < arr[mid]) {
+            max = mid - 1;
+        }
+    } return -1;
 }
 
 int main() {
-
-
     int num = 1957;
     std::string answer;
     printBase(num, 2, answer);
     std::cout << answer << std::endl;
+
+    int arr[] = {-1, 5, 8, 11, 22, 34};
+    int x = 11;
+
+    int location = binarySearch(arr, x, 0, 5);
+
+    std::cout << location << std::endl;
 }
