b60d00eb8479340b6fd558a364f5ca267ca574af
ethan-leonard <ethan.leonard821@gmail.com>
2024-10-24 10:27:31 -0700

    update 10.24

--- In-Class/10.24/main.cpp
+++ In-Class/10.24/main.cpp
@@ -52,6 +52,29 @@ int binarySearch(int arr[], int x, int min, int max) {
     } return -1;
 }
 
+/* Recursive Method
+int binarySearch(int arr[], int low, int high, int x)
+{
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return binarySearch(arr, low, mid - 1, x);
+
+        // Else the element can only be present
+        // in right subarray
+        return binarySearch(arr, mid + 1, high, x);
+    }
+  return -1;
+} */
+
 int main() {
     int num = 1957;
     std::string answer;
