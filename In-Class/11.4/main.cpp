4b73c76f14fc35dc6a48152323c7523caac7666c
ethan-leonard <ethan.leonard821@gmail.com>
2024-11-05 09:14:04 -0800

    update 11.5

---
+++ In-Class/11.4/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+template <class T>
+std::vector<T> readFileIn(std::string file_name) {
+    std::ifstream in_file;
+    T temp;
+    std::vector<T> vec;
+
+    in_file.open(file_name);
+    while (!in_file.eof()) {
+        in_file >> temp;
+        vec.push_back(temp);
+    } in_file.close();
+
+    return vec;
+}
+
+int main(int arg, char* argv[]) { 
+    std::string file_name;
+    std::vector<int> numbers;
+
+    std::cout << "Input file name: ";
+    std::cin >> file_name;
+    numbers = readFileIn<int>(file_name);
+
+    for (int i = 0; i < numbers.size(); i++) {
+        std::cout << (numbers.at(i)) << std::endl;
+    }
+
+}
